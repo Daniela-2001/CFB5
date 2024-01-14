81c342c577107f3a81b30477754d07b459e260a8
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-14 00:18:46 +0000

    CFB5

---
+++ aula05/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace aula05
+{
+    internal class Program
+    {
+        static void Main(){
+
+            int num = 10;
+
+            num ++;//num+=1; num=num+1; Incrementando(acrescentar) 1 no valor de 10
+
+            Console.WriteLine(num); 
+        }
+    }
+}
